adc6aebe81589ccee5968efb7e4bbf2991585110
Joshua Packer <jpacker@redhat.com>
2021-06-25 16:14:15 -0400

    Update package
    Signed-off-by: Joshua Packer <jpacker@redhat.com>

--- controller/clusterclaims-controller.go
+++ controller/clusterclaims-controller.go
@@ -1,15 +1,19 @@
 // Copyright Contributors to the Open Cluster Management project.
 
-package clusterclaims
+package controller
 
 import (
 	"context"
 	"crypto/sha256"
 
 	"github.com/go-logr/logr"
+	mcv1 "github.com/open-cluster-management/api/cluster/v1"
+	kacv1 "github.com/open-cluster-management/klusterlet-addon-controller/pkg/apis/agent/v1"
 	hivev1 "github.com/openshift/hive/apis/hive/v1"
 	corev1 "k8s.io/api/core/v1"
+	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
@@ -28,9 +32,8 @@ var hash = sha256.New()
 // ProviderCredentialSecretReconciler reconciles a Provider secret
 type ClusterClaimsReconciler struct {
 	client.Client
-	APIReader client.Reader
-	Log       logr.Logger
-	Scheme    *runtime.Scheme
+	Log    logr.Logger
+	Scheme *runtime.Scheme
 }
 
 func (r *ClusterClaimsReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
@@ -39,14 +42,65 @@ func (r *ClusterClaimsReconciler) Reconcile(ctx context.Context, req ctrl.Reques
 
 	var cc hivev1.ClusterClaim
 	if err := r.Get(ctx, req.NamespacedName, &cc); err != nil {
-		log.V(1).Info("Resource deleted")
+		log.V(0).Info("Resource deleted")
 
 		//TODO: cleaneUpManagedCluster( req.NamespacedName)
 
+		return ctrl.Result{}, nil
+	}
+
+	target := cc.ClusterName
+	log.V(0).Info("Initalize cluster: " + target + " if needed")
+
+	// ManagedCluster
+	var mc mcv1.ManagedCluster
+	err := r.Get(ctx, types.NamespacedName{Namespace: cc.Namespace, Name: target}, &mc)
+	if k8serrors.IsNotFound(err) {
+
+		log.V(0).Info("Create a new ManagedCluster resource and KlusterletAddonConfig")
+		mc.Name = target
+		mc.Spec.HubAcceptsClient = true
+		mc.ObjectMeta.Labels = map[string]string{"vendor": "OpenShift"}
+
+		if err = r.Create(ctx, &mc, &client.CreateOptions{}); err != nil {
+
+			log.V(2).Info("Could not create ManagedCluster resource: " + target)
+			return ctrl.Result{}, err
+		}
+
+	} else if err != nil {
+
+		log.V(1).Info("Error when attempting to retreive the ManagedCluster resource: " + target)
 		return ctrl.Result{}, err
 	}
 
-	log.V(1).Info("Create managedCluster kind if needed")
+	// KlusterletAddonConfig
+	var kac kacv1.KlusterletAddonConfig
+	err = r.Get(ctx, types.NamespacedName{Namespace: target, Name: target}, &kac)
+	if k8serrors.IsNotFound(err) {
+
+		log.V(0).Info("Create a new KlusterletAddonConfig resource")
+		kac.Name = target
+		kac.Namespace = target
+		kac.Spec.ClusterName = target
+		kac.Spec.ClusterNamespace = target
+		kac.Spec.ApplicationManagerConfig.Enabled = true
+		kac.Spec.CertPolicyControllerConfig.Enabled = true
+		kac.Spec.IAMPolicyControllerConfig.Enabled = true
+		kac.Spec.PolicyController.Enabled = true
+		kac.Spec.SearchCollectorConfig.Enabled = true
+
+		if err = r.Create(ctx, &kac, &client.CreateOptions{}); err != nil {
+
+			log.V(2).Info("Could not create KlusterletAddonConfig resource: " + target)
+			return ctrl.Result{}, err
+		}
+
+	} else if err != nil {
+
+		log.V(1).Info("Error when attempting to retreive the KlusterletAddonConfig resource: " + target)
+		return ctrl.Result{}, err
+	}
 
 	return ctrl.Result{}, nil
 }
--- go.mod
+++ go.mod
@@ -1,13 +1,23 @@
-module github.com/open-cluster-management/clusterclaims-controller
+module github.com/jnpacker/clusterclaims-controller
 
 go 1.16
 
 require (
 	github.com/go-logr/logr v0.4.0
+	github.com/open-cluster-management/api v0.0.0-20210527013639-a6845f2ebcb1
+	github.com/open-cluster-management/klusterlet-addon-controller v0.0.0-20210624203246-085d806736ce
 	github.com/openshift/hive/apis v0.0.0-20210624144808-697460baf215
 	go.uber.org/zap v1.17.0
 	k8s.io/api v0.21.2
 	k8s.io/apimachinery v0.21.2
-	k8s.io/client-go v0.21.2
+	k8s.io/client-go v12.0.0+incompatible
 	sigs.k8s.io/controller-runtime v0.9.2
+)
+
+replace (
+	k8s.io/api => k8s.io/api v0.20.0
+	k8s.io/client-go => k8s.io/client-go v0.20.0
+	sigs.k8s.io/controller-runtime => sigs.k8s.io/controller-runtime v0.6.2
+	github.com/go-logr/logr => github.com/go-logr/logr v0.2.1
+	github.com/go-logr/zapr => github.com/go-logr/zapr v0.2.0
 )
